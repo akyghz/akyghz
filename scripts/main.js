29e0dd058f1db44d29b3c5ef8a5627dbfcd0f343
ajitghz582 <ajitghz@gmail.com>
2015-08-24 00:00:28 +0530

    Signed-off-by: ajitghz582 <ajitghz@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -14,13 +14,19 @@ imag.onclick = function() {
 	}
 }
 
+/*changing head of my html page by getting input from user*/ 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Sun Rise, ' + myName;
+	if (myName == "") {
+		prompt('please enter a valid name');
+		localStorage.setItem('name', myName);
+		myHeading.innerHTML = 'Sun Rise ---	' + myName;
+	} else 
+  		myHeading.innerHTML = 'Sun Rise ---	' + myName;
 }
 
 if(!localStorage.getItem('name')) {
@@ -29,7 +35,20 @@ if(!localStorage.getItem('name')) {
   var storedName = localStorage.getItem('name');
   myHeading.innerHTML = 'Sun Rise, ' + storedName;
 }
-
+/*change username by clicking this button*/
 myButton.onclick = function() {
   setUserName();
 }
+
+function fakecounter(){
+
+//decrease/increase counter value (depending on perceived popularity of your site!)
+var decrease_increase = 50000;
+
+var counterdate = new Date();
+var currenthits = counterdate.getTime().toString();
+currenthits = parseInt(currenthits.substring(2,currenthits.length-4))+decrease_increase;
+
+document.write("You are visitor # <b>"+currenthits+"</b> to my site!")
+}
+fakecounter();
